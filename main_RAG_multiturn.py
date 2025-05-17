1eb46dba4db363cfbdc5a0cda4dd4dc31311ae19
Downy-newlearner <jdh251425142514@gmail.com>
2025-05-17 23:22:27 +0000

    제출 형식으로 정리

--- main_RAG_multiturn.py
+++ main_RAG_multiturn.py
@@ -9,17 +9,9 @@ from code.prompts.templates import TEMPLATES
 from code.utils.experiment import ExperimentRunner
 
 def main(num):
-
-    UPSTAGE_API_KEYS = [
-        "UPSTAGE_API_KEY1",
-        "UPSTAGE_API_KEY2",
-        "UPSTAGE_API_KEY3"
-    ]
-
-
     # API 키 로드
     load_dotenv()
-    api_key = os.getenv(UPSTAGE_API_KEYS[2])
+    api_key = os.getenv('UPSTAGE_API_KEYS')
     if not api_key:
         raise ValueError("API key not found in environment variables")
     
@@ -27,7 +19,7 @@ def main(num):
     base_config = ExperimentConfig(template_name='basic')
     
     # 데이터 로드
-    test = pd.read_csv(os.path.join(base_config.data_dir, f'test{num+1}.csv')) # test.csv에서 100개의 데이터를 샘플링한 데이터입니다.
+    test = pd.read_csv(os.path.join(base_config.data_dir, f'test.csv')) # test.csv에서 100개의 데이터를 샘플링한 데이터입니다.
     
     # 템플릿 선택
     template_name = 'few_shot6_ver2'
@@ -47,14 +39,14 @@ def main(num):
         'cor_sentence': test_results['cor_sentence']
     })
     
-    output.to_csv(f"1st_result_{template_name}_{num+1}.csv", index=False)
-    print(f"\n제출 파일이 생성되었습니다: 1st_result_{template_name}_{num+1}.csv")
+    output.to_csv(f"1st_result_{template_name}.csv", index=False)
+    print(f"\n제출 파일이 생성되었습니다: 1st_result_{template_name}.csv")
     print(f"사용된 템플릿: {template_name}")
     print(f"예측된 샘플 수: {len(output)}")
 
     # 유사도 비교 및 few-shot prompting
     # '1st_result_few_shot6_ver2.csv' 파일 로드
-    result_df = pd.read_csv(f"1st_result_{template_name}_{num+1}.csv")
+    result_df = pd.read_csv(f"1st_result_{template_name}.csv")
     results = []
     for _, row in tqdm(result_df.iterrows(), total=len(result_df)):
         text = row['cor_sentence']
@@ -67,9 +59,9 @@ def main(num):
 
     # 결과를 CSV 파일로 저장
     output_df = pd.DataFrame(results)
-    output_df.to_csv(f"submission_few_shot6_ver2_{num+1}.csv", index=False)
-    print(f"\n결과가 CSV 파일로 저장되었습니다: submission_few_shot6_ver2_{num+1}.csv")
+    output_df.to_csv(f"submission_few_shot6_ver2.csv", index=False)
+    print(f"\n결과가 CSV 파일로 저장되었습니다: submission_few_shot6_ver2.csv")
 
 
 if __name__ == "__main__":
-    main(8)
+    main(6)
